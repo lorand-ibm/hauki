ea193cbacbfcf7124fbf12b6b48749ef5b0f7ed2
iivoraitahila <79191921+iivoraitahila@users.noreply.github.com>
2021-10-26 16:23:40 +0300

    Update test_opening_hours_api.py

--- hours/tests/test_opening_hours_api.py
+++ hours/tests/test_opening_hours_api.py
@@ -19,7 +19,7 @@ def test_opening_hours_empty(admin_client):
         content_type="application/json",
     )
 
-    assert response.status_code == 209, "{} {}".format( #tahallinen virhe
+    assert response.status_code == 200, "{} {}".format( #tahallinen virhe code = 209
         response.status_code, response.data
     )
 
